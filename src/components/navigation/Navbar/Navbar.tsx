f5b6944242c80064acc5d3b604d891c1fb358fb5
Ilya Dvorchuk <iladvorchuk@gmail.com>
2024-07-03 22:57:40 +0300

    feat: add i18next

--- src/components/navigation/Navbar/Navbar.tsx
+++ src/components/navigation/Navbar/Navbar.tsx
@@ -3,14 +3,17 @@ import cls from './Navbar.module.scss'
 import {navItems} from "./data";
 import {MenuItem} from "@mui/material";
 import {Link} from "react-router-dom";
+import { useTranslation } from 'react-i18next';
 
 const Navbar = () => {
+    const {t} = useTranslation('main')
+
     return (
         <nav className={cls.Navbar}>
             {navItems.map(nav => (
                 <MenuItem key={nav.name} className={cls.navItem}>
                     <img src={nav.svg} alt={nav.name}/>
-                    <Link to={nav.url}>{nav.name}</Link>
+                    <Link to={nav.url}>{t(nav.name)}</Link>
                 </MenuItem>
             ))}
         </nav>
