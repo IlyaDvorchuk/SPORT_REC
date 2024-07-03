3d9ababcbfd0ba0638fc02552fc9a07b3e3e77e9
Ilya Dvorchuk <iladvorchuk@gmail.com>
2024-07-03 16:21:43 +0300

    feat: delete unnecessary styles

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,10 +1,39 @@
 import React from 'react';
+import cls from './Header.module.scss'
+
+import LogoSvg from '/src/assets/images/svg/logo.svg'
+import { AppBar, Toolbar, Typography, IconButton, Select, MenuItem } from '@mui/material';
+import Container from "../Container/Container";
 
 const Header = () => {
     return (
-        <header>
-            
-        </header>
+        <AppBar position="sticky" className={cls.appBar} color={'inherit'}>
+            <Container>
+                <Toolbar className={cls.toolbar}>
+                    <Typography variant="h1" className={cls.logo}>
+                        <img src={LogoSvg} alt="SPORT REC"/>
+                    </Typography>
+                    <Typography className={cls.navItem}>Спортивная борьба</Typography>
+                    <Typography className={cls.navItem}>Рейтинги</Typography>
+                    <Typography className={cls.navItem}>Соревнования</Typography>
+                    <Typography className={cls.navItem}>Организации</Typography>
+                    <Select
+                        value="RU"
+                        className={cls.languageSelect}
+                        variant="standard"
+                    >
+                        <MenuItem value="RU">RU</MenuItem>
+                        <MenuItem value="EN">EN</MenuItem>
+                    </Select>
+                    <IconButton color="inherit">
+                        {/*<NotificationsIcon />*/}
+                    </IconButton>
+                    <div className={cls.rightIcons}>
+                        <img src={LogoSvg} alt="logo" className={cls.logoImage} />
+                    </div>
+                </Toolbar>
+            </Container>
+        </AppBar>
     );
 };
 
