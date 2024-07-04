d7ba94579c43bea471672f143baca026159836be
Ilya Dvorchuk <iladvorchuk@gmail.com>
2024-07-04 23:16:15 +0300

    feat: add rtk query

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from "../../components/layout/Header/Header";
+import PostsComponent from "../../components/layout/Post/Post";
 
 const Main = () => {
     return (
         <>
             <Header/>
+            <PostsComponent/>
         </>
     );
 };
--- src/store/store.ts
+++ src/store/store.ts
@@ -2,6 +2,7 @@ import {combineReducers, configureStore} from '@reduxjs/toolkit';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import userSlice from "./reducers/UserSlice";
+import postService from "../services/simpleService";
 
 const persistConfig = {
     key: 'root',
@@ -10,6 +11,7 @@ const persistConfig = {
 
 const rootReducer = combineReducers({
     user: userSlice,
+    [postService.reducerPath]: postService.reducer
 });
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
@@ -17,6 +19,9 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 const setupStore = () => {
     return configureStore({
         reducer: persistedReducer,
+        middleware: (getDefaultMiddleware) =>
+            getDefaultMiddleware()
+                .concat(postService.middleware)
     });
 }
 
