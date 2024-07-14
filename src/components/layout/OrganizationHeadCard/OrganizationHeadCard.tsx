cf81533e98c89e0a5678aa47c4841fedb4b2e308
Ilya Dvorchuk <iladvorchuk@gmail.com>
2024-07-14 16:48:20 +0300

    feat: add avatar

--- src/components/layout/OrganizationHeadCard/OrganizationHeadCard.tsx
+++ src/components/layout/OrganizationHeadCard/OrganizationHeadCard.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import cls from './OrganizationHeadCard.module.scss'
-import {Box} from "@mui/material";
+import {Avatar, Box} from "@mui/material";
+
+import logoOrg from '/src/assets/images/raster/logo-org.png'
 
 const OrganizationHeadCard = () => {
     return (
         <Box className={cls.card}>
             <div className={cls.cardBackground}/>
+            <Box className={cls.cardBody}>
+                <Avatar
+                    src={logoOrg}
+                    sx={{width: 140, height: 140}}
+                    className={cls.avatar}
+
+                />
+            </Box>
         </Box>
     );
 };
